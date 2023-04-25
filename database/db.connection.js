fcbb5e1155c605483c6a8aef8e996040b0a092f3
Matin Askari <dev.mtnaskari@gmail.com>
2023-04-25 20:07:44 +0330

    feature(database): Add MySQL database connection

---
+++ database/db.connection.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("hw18_sequelize", "root", "123456789", {
+  host: "localhost",
+  port: 3307,
+  dialect: "mysql",
+  logging: false,
+});
+
+const checkDatabaseConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "[i] Connection has been established successfully to the MySQl DB."
+    );
+  } catch (error) {
+    console.error("[-] Unable to connect to the MySQL database:", error);
+    process.exit(1);
+  } finally {
+    sequelize.close();
+  }
+};
+
+module.exports = {
+  checkDatabaseConnection,
+};
