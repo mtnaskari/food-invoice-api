0cc9c5ae73a3fa6bcd183eab9ec0c5499ad5dba2
Matin Askari <dev.mtnaskari@gmail.com>
2023-04-25 20:46:47 +0330

    feature(models): Add food model

---
+++ database/models/food.model.js
@@ -0,0 +1,36 @@
+const { sequelize } = require("../db.connection");
+const { DataTypes } = require("sequelize");
+
+const Food = sequelize.define(
+  "Food",
+  {
+    // Model attributes are defined here
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: "Foods name must be between 3 and 30 characters long",
+        },
+      },
+      notNull: {
+        msg: "Food name is required",
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    notNull: {
+      msg: "Food price is required",
+    }
+  },
+  {
+    timestamps: true,
+    tableName: "foods",
+  }
+);
+
+
+module.exports = Food;
