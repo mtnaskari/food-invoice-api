08560d65c52710f9bd960a2197d8ce591be34202
Matin Askari <dev.mtnaskari@gmail.com>
2023-04-25 20:47:10 +0330

    feature(models): Add invoice model

---
+++ database/models/invoice.model.js
@@ -0,0 +1,31 @@
+const { sequelize } = require("../db.connection");
+const { DataTypes } = require("sequelize");
+
+const Invoice = sequelize.define(
+  "Invoice",
+  {
+    trackingCode: {
+      type: DataTypes.STRING(12),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [12, 12],
+          msg: "Invoice tracking code must be 12 characters long",
+        },
+        notNull: {
+          msg: "Invoice tracking code is required",
+        },
+        notEmpty: {
+          msg: "Invoice tracking code is required",
+        },
+      },
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "invoices",
+  }
+);
+
+module.exports = Invoice;
